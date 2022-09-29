923f26a480fd8122064ae89d81334465946b78f7
Vitaliy <vitalykd.232@gmail.com>
2022-09-29 16:31:50 +0500

    Say Hi, to people!

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "алена")
+{
+    Console.WriteLine("Ура, это же Алена!");
+}
+else
+{
+  Console.Write("Привет, ");
+  Console.WriteLine(username); 
+}
